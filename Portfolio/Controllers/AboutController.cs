c71e4bf060b7d28c2c9e50b5bf729282b3cf155e
Abdullah Dagli <abdullah.dagli@outlook.com>
2026-06-28 16:18:03 +0300

    Refresh

--- Portfolio/Controllers/AboutController.cs
+++ Portfolio/Controllers/AboutController.cs
@@ -1,21 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portfolio.Data.Context;
+using Portfolio.Data.Entites;
 
 namespace Portfolio.Controllers
 {
 	public class AboutController : Controller
 	{
+
+
 		private readonly AppDbContext _context;
+
 
 		public AboutController(AppDbContext context)
 		{
 			_context = context;
 		}
 
-		public IActionResult Index()
+		//sadece bu sınıf üzerinden erişilebilen bir fieldir. yani controller sınıfından bir index sayfası ayağı kalktığı zaman  biz bir constractot oluşturup burası otomatik olarak ayağı kaldırmak isteteceğimiz bir alandır. aslında constractor oluştumamızdaki mahiyet.
+
+
+
+		public IActionResult Index() // index içerisindeki bizim amacımız neydi verileri listelemek/getirmek.
 		{
-			var about = _context.Abouts.FirstOrDefault();
+			var about = _context.Abouts.FirstOrDefault(); // burdaki ilk veriyi yani tek bir değer getirmek isteriz.
 			return View(about);
+		}
+
+		[HttpGet]
+		public IActionResult CreateAbout()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult CreateAbout(About about)
+		{
+		 	_context.Abouts.Add(about);
+			_context.SaveChanges();
+			return RedirectToAction("Index");
 		}
 	}
 
