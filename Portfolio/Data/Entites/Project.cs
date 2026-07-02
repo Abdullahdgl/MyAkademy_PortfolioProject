813f3cd616dd66a1d6881503ed154e5e6226cc7c
Abdullah Dagli <abdullah.dagli@outlook.com>
2026-07-02 22:18:21 +0300

    Project Güncelleme seri 6

--- Portfolio/Data/Entites/Project.cs
+++ Portfolio/Data/Entites/Project.cs
@@ -1,14 +1,24 @@
-﻿namespace Portfolio.Data.Entites
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Data.Entites
 {
 	public class Project
 	{
 		public int Id { get; set; }
+		[Required(ErrorMessage ="Görsel URL Boş Bırakılamaz.")]
 		public string ImageUrl { get; set; }
+		[Required(ErrorMessage = "Proje adı Boş Bırakılamaz.")]
+		[MinLength(3,ErrorMessage ="Proje Adı En az 3 karakter olmalıdır.")]
 		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Proje adı Boş Bırakılamaz.")]
+		[MaxLength(100, ErrorMessage ="Proje Açıklaması En fazla 100 karakter olmalıdır. ")]
 		public string Description { get; set; }
+
+		[Required(ErrorMessage = "Github url Boş Bırakılamaz.")]
 		public string GithubUrl { get; set; }
 
-		public List<ProjectTechStack> ProjectTechStacks { get; set; }
+		public List<ProjectTechStack>? ProjectTechStacks { get; set; }
 
 
 	}
