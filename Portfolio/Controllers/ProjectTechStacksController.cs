89f9a4ec9b58fffbbfcae81e87b9a450480855d1
Abdullah Dagli <abdullah.dagli@outlook.com>
2026-07-20 08:16:22 +0300

    admin_added

--- Portfolio/Controllers/ProjectTechStacksController.cs
+++ Portfolio/Controllers/ProjectTechStacksController.cs
@@ -16,14 +16,26 @@ namespace Portfolio.Controllers
 		}
 
 		//eager loading:Herşeyi tek seferde getirmek için kullanılır. yani birden fazla tabloyu tek seferde getirmek için kullanılır. lazy loading: sadece ihtiyacımız olan veriyi getirmek için kullanılır. yani bir tabloyu getirmek için kullanılır. explicit loading: sadece ihtiyacımız olan veriyi getirmek için kullanılır. yani bir tabloyu getirmek için kullanılır. SQL deki inner join gibi düşünebiliriz. yani bir tabloyu getirmek için kullanılır.
+		//public IActionResult Index()
+		//{
+		//	var projectTechStacks = _context.ProjectTechStacks
+		//		.Include(x => x.Project)
+		//		.Include(y => y.TechStack).ToList();
+
+		//	return View(projectTechStacks);
+		//}
+
 		public IActionResult Index()
 		{
-			var projectTechStacks = _context.ProjectTechStacks
-				.Include(x => x.Project)
-				.Include(y => y.TechStack).ToList();
+			var projects = _context.Projects
+				.Include(x => x.ProjectTechStacks)
+				.ThenInclude(x => x.TechStack)
+				.ToList();
 
-			return View(projectTechStacks);
+			return View(projects);
 		}
+
+
 
 		[HttpGet]
 		public IActionResult Create()
@@ -46,13 +58,64 @@ namespace Portfolio.Controllers
 
 			return View();
 		}
+
+
+
+		//old
+
+		//[HttpPost]
+		//public IActionResult Create(ProjectTechStack projectTechStack)
+		//{
+
+		//	_context.ProjectTechStacks.Add(projectTechStack);
+		//	_context.SaveChanges();
+		//	return RedirectToAction("Index");
+		//}
+
 		[HttpPost]
-		public IActionResult Create(ProjectTechStack projectTechStack)
+		public IActionResult Create(int ProjectId, List<int> SelectedTechStackIds)
 		{
-		
-			_context.ProjectTechStacks.Add(projectTechStack);
-			_context.SaveChanges();
+			if (SelectedTechStackIds != null)
+			{
+				foreach (var techId in SelectedTechStackIds)
+				{
+					_context.ProjectTechStacks.Add(new ProjectTechStack
+					{
+						ProjectId = ProjectId,
+						TechStackId = techId
+					});
+				}
+				_context.SaveChanges();
+			}
+
 			return RedirectToAction("Index");
+
+
+
+
 		}
+
+
+
+
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+		//public IActionResult Delete(int projectId, int techStackId)
+		//{
+		//	var projectTechStack = _context.ProjectTechStacks
+		//		.FirstOrDefault(x => x.ProjectId == projectId &&
+		//							 x.TechStackId == techStackId);
+
+		//	if (projectTechStack == null)
+		//	{
+		//		return NotFound();
+		//	}
+
+		//	_context.ProjectTechStacks.Remove(projectTechStack);
+		//	_context.SaveChanges();
+
+		//	return RedirectToAction(nameof(Index));
+		//}
+
 	}
 }
