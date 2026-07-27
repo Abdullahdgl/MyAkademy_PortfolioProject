0368abb577886a1273ed18ed102866df90ea360e
Abdullah Dagli <abdullah.dagli@outlook.com>
2026-07-27 21:24:51 +0300

    refresh

---
+++ Portfolio/ViewComponents/Default-Index/_DefaultPortfolioViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Portfolio.Data.Context;
+
+namespace Portfolio.ViewComponents.Default_Index
+{
+	public class _DefaultPortfolioViewComponent : ViewComponent
+	{
+
+		private readonly AppDbContext _context;
+
+		public _DefaultPortfolioViewComponent(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+
+			var contectInfo = _context.Projects.Include(X=>X.ProjectTechStacks).ThenInclude(X=>X.TechStack).ToList();
+
+			return View(contectInfo);
+		}
+
+	}
+}
