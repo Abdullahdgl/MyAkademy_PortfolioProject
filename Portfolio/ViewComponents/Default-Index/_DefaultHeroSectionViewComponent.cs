d3fc3523fca1a322d8c19c2aa07770dd7bb6f482
Abdullah Dagli <abdullah.dagli@outlook.com>
2026-07-27 08:16:50 +0300

    case 1 edit

---
+++ Portfolio/ViewComponents/Default-Index/_DefaultHeroSectionViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Data.Context;
+
+namespace Portfolio.ViewComponents.Default_Index
+{
+	public class _DefaultHeroSectionViewComponent : ViewComponent
+	{
+		private readonly AppDbContext _context;
+
+		public _DefaultHeroSectionViewComponent(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var abaout = _context.Banners.FirstOrDefault();
+			return View(abaout);
+		}
+	}
+}
---
+++ Portfolio/ViewComponents/Default-Index/_DefaultNavigationViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Data.Context;
+
+namespace Portfolio.ViewComponents.Default_Index
+{
+	public class _DefaultNavigationViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+
+			return View();
+		}
+	}
+}
