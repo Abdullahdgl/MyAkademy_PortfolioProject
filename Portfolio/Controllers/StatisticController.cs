91a03ac26ac0d934ff32ce3160ff7c1d7ff31afe
Abdullah Dagli <abdullah.dagli@outlook.com>
2026-08-02 22:38:24 +0300

    Statistic create update

---
+++ Portfolio/Controllers/StatisticController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Data.Context;
+
+namespace Portfolio.Controllers
+{
+	public class StatisticController : Controller
+	{
+		private readonly AppDbContext context;
+
+		public StatisticController(AppDbContext context)
+		{
+			this.context = context;
+		}
+
+		public IActionResult Index()
+		{
+			ViewBag.v1 = context.Skills.Count();
+			ViewBag.v2 = context.UserMessages.Count();
+			ViewBag.v3 = context.UserMessages.Where(x => x.IsRead == false).Count();
+			ViewBag.v4 = context.UserMessages.Where(x => x.IsRead == true).Count();
+
+			return View();
+		}
+	}
+}
